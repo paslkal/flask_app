f1ed579b30a8863eea6da99432c4fe4ccf060611
paslkal <164247200+paslkal@users.noreply.github.com>
2024-08-26 23:44:54 +0300

    create tests for creating and interacting with database

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from db import get_messages, delete_message, add_message
+from db import get_tasks, delete_task, add_task
 
+"""
 print(get_messages())
 
 print((add_message({'title':'title 1', 'content': 'ndscnclNC;NSC;NDSA'})))
 
 print(delete_message(5))
+"""
+# print(get_tasks())
+
+# print((add_task({'title':'title 1 checked = True', 'checked': True})))
+# print((add_task({'title':'title 2 - checked = False', 'checked': False})))
+
+# print(delete_task(1))
+
+
 
