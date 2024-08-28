51fb96bae19d8f1f9c7ee1573ceda68a8f77184b
paslkal <164247200+paslkal@users.noreply.github.com>
2024-08-28 21:31:54 +0300

    use sqlalchemy instead of psycopg2

--- db.py
+++ db.py
@@ -1,227 +1,144 @@
-from psycopg2 import connect
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Text
+from sqlalchemy.orm import sessionmaker, declarative_base
 from env import port, host, database, user, password
 
+Base = declarative_base()
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(50), nullable=False)
+    content = Column(Text, nullable=False)
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(50), nullable=False)
+    content = Column(Text)
+    checked = Column(Boolean, nullable=False)
+
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
 def create_tables():
-    with connect(
-        port=port, 
-        host=host, 
-        database=database, 
-        user=user, 
-        password=password
-    ) as conn:
-        
-        conn.autocommit = True
+    session = SessionLocal()
+    try:
+        message1 = Message(title='Breakfast', content='Eggs, bread, milk')
+        message2 = Message(title='Sport', content='Football, basketball, formula 1')
+        task1 = Task(title='Make breakfast', checked=True)
+        task2 = Task(title='Play football', checked=False)
 
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                create table messages(
-                id bigserial not null primary key,
-                title varchar(50) not null,
-                content text not null
-                );
-                """
-            )
+        session.add_all([message1, message2, task1, task2])
+        session.commit()
 
-            cur.execute(
-                """
-                create table tasks(
-                id bigserial not null primary key,
-                title varchar(50) not null,
-                content text,
-                checked boolean not null
-                );
-                """
-            )
+        messages = get_messages()
+        tasks = get_tasks()
 
-            cur.execute(
-                """
-                insert into messages (title, content) 
-                values 
-                ('Breakfast', 'Eggs, bread, milk'),
-                ('Sport', 'Football, basketball, formula 1');
-                """
-            )
-
-            cur.execute(
-                """
-                insert into tasks (title, checked) 
-                values 
-                ('Make breakfast', TRUE),
-                ('Play football', FALSE);
-                """
-            )
-
-            cur.execute('select * from messages')
-
-            messages = cur.fetchall()
-
-            cur.execute('select * from tasks')
-
-            tasks = cur.fetchall()
-
-            print(f'{messages=}, {tasks=}')
-
+        print(f'{messages=}\n')
+        print(f'{tasks=}\n')
+    finally:
+        session.close()
 
 def get_messages():
-    with connect(
-        port=port, 
-        host=host, 
-        database=database, 
-        user=user, 
-        password=password
-    ) as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM messages")
-            rows = cur.fetchall()
+    session = SessionLocal()
+    try:
+        messages = session.query(Message).all()
+        dict_messages = [message.__dict__ for message in messages]
+        real_messages = [
+            {
+                'id': message['id'],
+                'title': message['title'],
+                'content': message['content']
+            }
 
-            description = cur.description
+            for message in dict_messages
+        ]
 
-            if (not rows) or description is None:
-                return []
-
-            colnames = [desc[0] for desc in description]
-
-            messages = [dict(zip(colnames, row)) for row in rows]
-
-            return messages
-
-
+        return real_messages
+    finally:
+        session.close()
 
 def add_message(message):
-    with connect(
-        port=port, 
-        host=host, 
-        database=database, 
-        user=user, 
-        password=password
-    ) as conn:
-        with conn.cursor() as cur:
-            title = message['title']    
-            content = message['content']    
-            cur.execute(
-                """
-                insert into messages (title, content)
-                values (%s, %s)
-                """, (title, content)
-            )
-
-            conn.commit()
-
-            return get_messages()
+    session = SessionLocal()
+    try:
+        new_message = Message(title=message['title'], content=message['content'])
+        session.add(new_message)
+        session.commit()
+        return get_messages()
+    finally:
+        session.close()
 
 def delete_message(message_id):
-    with connect(
-        port=port, 
-        host=host, 
-        database=database, 
-        user=user, 
-        password=password
-    ) as conn:
-        
-        with conn.cursor() as cur:
-            cur.execute('delete from messages where id = %s;', (message_id,))
-
-            conn.commit()
-            
-            return get_messages()
-
+    session = SessionLocal()
+    try:
+        message = session.query(Message).filter(Message.id == message_id).first()
+        if message:
+            session.delete(message)
+            session.commit()
+        return get_messages()
+    finally:
+        session.close()
 
 def get_tasks():
-    with connect(
-        port=port, 
-        host=host, 
-        database=database, 
-        user=user, 
-        password=password
-    ) as conn:
-
-        with conn.cursor() as cur:
-            cur.execute('select * from tasks;')
-
-            rows = cur.fetchall()
-
-            description = cur.description
-
-            if (not rows) or description is None:
-                return []
-            
-            colnames = [desc[0] for desc in description]
-
-            tasks = [dict(zip(colnames, row)) for row in rows]
-
-            conn.commit()
-            
-            sorted_tasks = sorted(tasks, key=lambda task: task['id'])
-
-            return sorted_tasks
-
-
+    session = SessionLocal()
+    try:
+        tasks = session.query(Task).all()
+        sorted_tasks = sorted(tasks, key=lambda task: task.__dict__['id'])
+        dict_tasks = [task.__dict__ for task in sorted_tasks]
+        real_tasks = [
+            dict(
+                id=task['id'],
+                title=task['title'],
+                content=task['content'],
+                checked=task['checked']
+            ) for task in dict_tasks
+        ]
+        return real_tasks
+    finally:
+        session.close()
 
 def add_task(task):
-    with connect(
-        port=port, 
-        host=host, 
-        database=database, 
-        user=user, 
-        password=password
-    ) as conn:
-        with conn.cursor() as cur:
-            title = task['title']    
-            content = task['content']
-            checked = task['checked']    
-            cur.execute(
-                """
-                insert into tasks (title, content, checked)
-                values (%s, %s, %s)
-                """, (title, content, checked)
-            )
-
-            conn.commit()
-
-            return get_tasks()
+    session = SessionLocal()
+    try:
+        new_task = Task(
+            title=task['title'], 
+            content=task.get('content', ''), 
+            checked=task['checked']
+        )
+        session.add(new_task)
+        session.commit()
+        return get_tasks()
+    finally:
+        session.close()
 
 def delete_task(task_id):
-    with connect(
-        port=port, 
-        host=host, 
-        database=database, 
-        user=user, 
-        password=password
-    ) as conn:
-        
-        with conn.cursor() as cur:
-            cur.execute('delete from tasks where id = %s;', (task_id,))
-
-            conn.commit()
-            
-            return get_tasks()
-
+    session = SessionLocal()
+    try:
+        task = session.query(Task).filter(Task.id == task_id).first()
+        if task:
+            session.delete(task)
+            session.commit()
+        return get_tasks()
+    finally:
+        session.close()
 
 def change_checked_value(task_id, checked):
-    with connect(
-        port=port, 
-        host=host, 
-        database=database, 
-        user=user, 
-        password=password
-    ) as conn:
-        
-        with conn.cursor() as cur:
-            cur.execute(
-                'update tasks set checked = %s where id = %s;',
-                (checked, task_id)
-            )
-
-            conn.commit()
-            
-            tasks = get_tasks()
-
-            print(tasks)
-
-            return tasks
-
-
+    session = SessionLocal()
+    try:
+        task = session.query(Task).filter(Task.id == task_id).first()
+        if task:
+            task.checked = checked
+            session.commit()
+        return get_tasks()
+    finally:
+        session.close()
 
 if __name__ == '__main__':
     create_tables()
